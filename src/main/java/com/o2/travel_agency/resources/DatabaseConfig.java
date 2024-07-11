3ac285b00cf2a6355a26b6bd3016b43e43e5ad05
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-07-11 17:17:51 -0500

    feat: add utils and hexagonal of plane

---
+++ src/main/java/com/o2/travel_agency/resources/DatabaseConfig.java
@@ -0,0 +1,15 @@
+package com.o2.travel_agency.resources;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+    private static final String URL = "jdbc:mysql://localhost:3306/sales";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
